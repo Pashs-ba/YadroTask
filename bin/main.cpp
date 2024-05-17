654747264968e8756c170ea503c214c71a7f3553
Pashs-ba <pashs.ba@gmail.com>
2024-05-17 17:53:52 +0300

    fix SitEvent

--- bin/main.cpp
+++ bin/main.cpp
@@ -2,6 +2,9 @@
 #include "lib/events/IncomeEvent/IncomeEvent.h"
 #include "lib/utils/time_converter/TimeConverter.h"
 #include "lib/manager/Manager.h"
+#include "lib/events/SitEvent/SitEvent.h"
+#include "lib/events/LeftEvent/LeftEvent.h"
+#include "lib/events/WaitEvent/WaitEvent.h"
 
 int main() {
   Manager manager{
@@ -10,14 +13,36 @@ int main() {
       utils::TimeConverter::toMinutes("09:30").value(),
       utils::TimeConverter::toMinutes("21:30").value()
   };
-  std::optional<std::unique_ptr<EventBase>> result = manager.submit(
+  manager.submit(
       std::make_unique<IncomeEvent>(
           "09:40",
           std::string_view("pashs.ba")));
+  manager.submit(
+      std::make_unique<SitEvent>(
+          "09:50",
+          std::string_view("pashs.ba"),
+          0));
+  manager.submit(
+      std::make_unique<IncomeEvent>(
+          "09:50",
+          std::string_view("pashs.ba2")));
+  manager.submit(
+      std::make_unique<WaitEvent>(
+          "09:50",
+          std::string_view("pashs.ba2")));
+  std::optional<std::unique_ptr<EventBase>> result = manager.submit(
+      std::make_unique<LeftEvent>(
+          "11:40",
+          std::string_view("pashs.ba")));
+  std::optional<std::unique_ptr<EventBase>> result2 = manager.submit(
+      std::make_unique<LeftEvent>(
+          "11:50",
+          std::string_view("pashs.ba2")));
   if (result.has_value()) {
     std::cout << *(result.value());
-  }else{
-    std::cout<<"Nothing!";
+  } else {
+    std::cout << "Nothing!";
   }
+
   return 0;
 }
--- lib/events/SitEvent/SitEvent.cpp
+++ lib/events/SitEvent/SitEvent.cpp
@@ -10,14 +10,20 @@ SitEvent::submit(utils::data::Context& context) const noexcept {
   if (context.tables[table_id_].busy) {
     return std::make_unique<ErrorEvent>(time_, "PlaceIsBusy");
   }
+  auto current_time = utils::TimeConverter::toMinutes(time_).value();
   if (context.current_users[client_] != -1) {
     auto& prev_table = context.tables[context.current_users[client_]];
     prev_table.busy = false;
     prev_table.busy_time_total +=
-        utils::TimeConverter::toMinutes(time_).value() - prev_table.busy_from;
+        current_time - prev_table.busy_from;
+    prev_table.revenue += context.cost_of_hour *
+        (current_time - prev_table.busy_from) / 60;
+    if ((current_time - prev_table.busy_from) % 60) {
+      prev_table.revenue += context.cost_of_hour;
+    }
   }
+  context.tables[table_id_].busy_from = current_time;
   context.tables[table_id_].busy = true;
-  context.tables[table_id_].busy_from = utils::TimeConverter::toMinutes(time_).value();
   context.current_users[client_] = table_id_;
   return std::nullopt;
 }
